38223611e7c2de8e375ff2ce790b597fb6419455
AleksBartov <37273772+AleksBartov@users.noreply.github.com>
2025-02-18 23:30:44 +0300

    font and foto downloaded

--- app/index.tsx
+++ app/index.tsx
@@ -1,53 +1,39 @@
+import React from "react";
 import {
   Canvas,
-  Rect,
+  Fill,
   Shader,
   Skia,
+  Text,
   useClock,
+  useFont,
 } from "@shopify/react-native-skia";
-import { useWindowDimensions } from "react-native";
-import React from "react";
+import { useDerivedValue } from "react-native-reanimated";
 
-export default function Index() {
-  const { width, height } = useWindowDimensions();
-  const clock = useClock(); // Хук для получения времени
+const source = Skia.RuntimeEffect.Make(`
+  uniform float time;
 
-  const source = Skia.RuntimeEffect.Make(`
-    uniform vec2 iResolution; // Разрешение экрана
-    uniform float iTime;      // Время для анимации
-
-    float f(vec3 p) {
-      p.z -= iTime * 10.;
-      float a = p.z * .1;
-      p.xy *= mat2(cos(a), sin(a), -sin(a), cos(a));
-      return .1 - length(cos(p.xy) + sin(p.yz);
+    vec4 main(vec2 fragCoord) {
+      float fade = clamp((fragCoord.x / 300.0) - (time * 0.001), 0.0, 1.0);
+      return vec4(1.0, 1.0, 1.0, 1.0 - fade); // Белый цвет с изменяющейся прозрачностью
     }
+`)!;
 
-    vec4 main(vec2 fragcoord) { 
-      vec3 d = .5 - fragcoord.xy1 / iResolution.y;
-      vec3 p = vec3(0);
-      for (int i = 0; i < 32; i++) {
-        p += f(p) * d;
-      }
-      return ((sin(p) + vec3(2, 5, 9)) / length(p)).xyz1;
-    }
-  `);
-
-  if (!source) {
-    return null; // Если шейдер не удалось создать, ничего не рендерим
-  }
-
+const Index = () => {
+  const clock = useClock();
+  const fontSize = 32;
+  const font = useFont(require("./../assets/fonts/Ponomar.otf"), fontSize);
+  const uniforms = useDerivedValue(
+    () => ({ time: clock.value / 1000 }),
+    [clock]
+  );
   return (
     <Canvas style={{ flex: 1 }}>
-      <Rect x={0} y={0} width={width} height={height}>
-        <Shader
-          source={source}
-          uniforms={{
-            iResolution: [width, height], // Разрешение экрана
-            iTime: clock.value / 1000, // Время для анимации в секундах
-          }}
-        />
-      </Rect>
+      <Text x={50} y={200} text="Борис Бартов 100 лет" font={font}>
+        <Shader source={source} uniforms={uniforms} />
+      </Text>
     </Canvas>
   );
-}
+};
+
+export default Index;
